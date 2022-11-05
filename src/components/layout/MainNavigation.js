65b87d2d476a011d6597c5be4ab5d56e961390c6
gbstorey <gbstorey@asu.edu>
2022-11-04 17:44:19 -0700

    Nested routes, imperative navigation, prompts, query parameters, useHistory, useLocation, http in routes

---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,28 @@
+import classes from "./MainNavigation.module.css";
+import { NavLink } from "react-router-dom";
+
+const MainNavigation = () => {
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>Great Quotes</div>
+      <nav className={classes.nav}>
+        <div>
+          <ul>
+            <li>
+              <NavLink to={"/quotes"} activeClassName={classes.active}>
+                All Quotes
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={"/new-quote"} activeClassName={classes.active}>
+                New Quote
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
